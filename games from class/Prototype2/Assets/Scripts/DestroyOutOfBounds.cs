9eac0e42b8133ef6465991b4eb01f712328b2912
EggSurger <113132105+EggSurger@users.noreply.github.com>
2022-10-25 10:55:48 -0600

    finished game and started chalenge

--- games from class/Prototype2/Assets/Scripts/DestroyOutOfBounds.cs
+++ games from class/Prototype2/Assets/Scripts/DestroyOutOfBounds.cs
@@ -22,7 +22,9 @@ public class DestroyOutOfBounds : MonoBehaviour
         } else if (transform.position.z < lowerBound)
         {
             Destroy(gameObject);
+            Debug.Log("Game Over!");
         }
+        
         
     }
 }
