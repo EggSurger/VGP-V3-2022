178e1aed98b9bfac3b3687a26d4e1826bb4f4e7f
EggSurger <113132105+EggSurger@users.noreply.github.com>
2022-10-26 10:58:46 -0600

    finished chalenge

--- games from class/Fetch/Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ games from class/Fetch/Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -9,6 +9,7 @@ public class SpawnManagerX : MonoBehaviour
     private float spawnLimitXLeft = -22;
     private float spawnLimitXRight = 7;
     private float spawnPosY = 30;
+    
 
     private float startDelay = 1.0f;
     private float spawnInterval = 4.0f;
@@ -24,9 +25,10 @@ public class SpawnManagerX : MonoBehaviour
     {
         // Generate random ball index and random spawn position
         Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
+        
 
         // instantiate ball at random spawn location
-        Instantiate(ballPrefabs[0], spawnPos, ballPrefabs[0].transform.rotation);
+        Instantiate(ballPrefabs[Random.Range(0, ballPrefabs.Length)], spawnPos, ballPrefabs[Random.Range(0, ballPrefabs.Length)].transform.rotation);
     }
 
 }
