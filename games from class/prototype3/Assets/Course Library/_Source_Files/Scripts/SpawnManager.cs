82195100a3144c4063ba0e0b0a749bac27384d7a
EggSurger <113132105+EggSurger@users.noreply.github.com>
2022-11-14 10:33:12 -0700

    made stone fence stop spawning

--- games from class/prototype3/Assets/Course Library/_Source_Files/Scripts/SpawnManager.cs
+++ games from class/prototype3/Assets/Course Library/_Source_Files/Scripts/SpawnManager.cs
@@ -8,22 +8,26 @@ public class SpawnManager : MonoBehaviour
     private Vector3 spawnpos = new Vector3(25, 0, 0);
     private float startDelay = 2;
     private float repeatRate = 2;
+    private PlayerControler playerControllerScript;
     // Start is called before the first frame update
     void Start()
     {
         InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerControler>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (trans)
     }
 
 
 
     void SpawnObstacle()
     {
+        if (playerControllerScript.gameOver == false){
         Instantiate(obstaclePrefab, spawnpos, obstaclePrefab.transform.rotation);
+        }
     }
 }
