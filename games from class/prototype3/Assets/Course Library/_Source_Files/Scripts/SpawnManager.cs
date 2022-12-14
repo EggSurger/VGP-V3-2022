50ee53a0bdb189cd5e45073ad13d67251f610b6f
EggSurger <113132105+EggSurger@users.noreply.github.com>
2022-12-14 10:59:28 -0700

    fixing bugs

--- games from class/prototype3/Assets/Course Library/_Source_Files/Scripts/SpawnManager.cs
+++ games from class/prototype3/Assets/Course Library/_Source_Files/Scripts/SpawnManager.cs
@@ -5,8 +5,7 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject[] ObstacleList;
-    public GameObject obstaclePrefab;
-    public GameObject obstaclePrefab2;
+ 
     private Vector3 spawnpos = new Vector3(25, 0, 0);
     private float startDelay = 2;
     private float repeatRate = 2;
@@ -18,6 +17,7 @@ public class SpawnManager : MonoBehaviour
         
         InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
         playerControllerScript = GameObject.Find("Player").GetComponent<PlayerControler>();
+        
     }
 
     // Update is called once per frame
@@ -34,7 +34,7 @@ public class SpawnManager : MonoBehaviour
     void SpawnObstacle()
     {
         if (playerControllerScript.gameOver == false){
-        Instantiate(ObstacleList[Random.Range(obstaclePrefab,obstaclePrefab2)], spawnpos, obstaclePrefab.transform.rotation);
+        Instantiate(ObstacleList[Random.Range(0,ObstacleList.Length)], spawnpos, transform.rotation);
         }
     }
 }
