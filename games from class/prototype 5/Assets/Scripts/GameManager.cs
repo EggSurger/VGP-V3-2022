d4b58f5bebec4e65f1940b6f250a80d8f44b1148
EggSurger <113132105+EggSurger@users.noreply.github.com>
2023-01-17 10:58:35 -0700

    made game over work

--- games from class/prototype 5/Assets/Scripts/GameManager.cs
+++ games from class/prototype 5/Assets/Scripts/GameManager.cs
@@ -11,15 +11,16 @@ public class GameManager : MonoBehaviour
     public List<GameObject> targets;
     private float spawnRate = 1.0f;
     private int score;
+    public bool isGameActive;
 
     // Start is called before the first frame update
     void Start()
     {
+        isGameActive = true;
         StartCoroutine(SpawnTarget());
         score = 0;
         UpdateScore(0);
         
-        gameOverText.gameObject.SetActive(true);
     }
 
     // Update is called once per frame
@@ -28,7 +29,7 @@ public class GameManager : MonoBehaviour
         
     }
     IEnumerator SpawnTarget()
-    {while (true){
+    {while (isGameActive){
         yield return new WaitForSeconds(spawnRate);
         int index = Random.Range(0, targets.Count);
         Instantiate(targets[index]);
@@ -37,5 +38,13 @@ public class GameManager : MonoBehaviour
     public void UpdateScore(int scoreToAdd){
         scoreText.text = "Score:" + score;
         score += scoreToAdd;
+
     }
+    public void GameOver()
+    {
+        gameOverText.gameObject.SetActive(true);
+        isGameActive = false;
+
+    }
+
 }
--- games from class/prototype 5/Assets/Scripts/Target.cs
+++ games from class/prototype 5/Assets/Scripts/Target.cs
@@ -23,12 +23,18 @@ public class Target : MonoBehaviour
         gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
     }
 private void OnMouseDown(){
+    if(gameManager.isGameActive){
     Destroy(gameObject);
     Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
     gameManager.UpdateScore(pointValue);
+    }
 }
 private void OnTriggerEnter(Collider other){
     Destroy(gameObject);
+    if(!gameObject.CompareTag("Bad"))
+    {
+        gameManager.GameOver();
+    }
 }
 
 
