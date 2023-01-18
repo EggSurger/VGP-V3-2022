57141022c3593c0caa1c74fe20d07acdc8c4def5
EggSurger <113132105+EggSurger@users.noreply.github.com>
2023-01-18 11:41:33 -0700

    adding title screne

--- games from class/prototype 5/Assets/Scripts/GameManager.cs
+++ games from class/prototype 5/Assets/Scripts/GameManager.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 public class GameManager : MonoBehaviour
 
 {
@@ -12,6 +14,7 @@ public class GameManager : MonoBehaviour
     private float spawnRate = 1.0f;
     private int score;
     public bool isGameActive;
+    public Button restartButton;
 
     // Start is called before the first frame update
     void Start()
@@ -44,7 +47,12 @@ public class GameManager : MonoBehaviour
     {
         gameOverText.gameObject.SetActive(true);
         isGameActive = false;
+        restartButton.gameObject.SetActive(true);
 
     }
+    public void RestartGame(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    
 
 }
