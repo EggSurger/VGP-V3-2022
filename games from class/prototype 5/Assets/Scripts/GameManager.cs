7c5023e1dab81e9109f68f5390d8da17b848a1c4
EggSurger <113132105+EggSurger@users.noreply.github.com>
2023-01-19 11:40:30 -0700

    made title screen buttons start the game

--- games from class/prototype 5/Assets/Scripts/GameManager.cs
+++ games from class/prototype 5/Assets/Scripts/GameManager.cs
@@ -19,10 +19,7 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        isGameActive = true;
-        StartCoroutine(SpawnTarget());
-        score = 0;
-        UpdateScore(0);
+      
         
     }
 
@@ -53,6 +50,11 @@ public class GameManager : MonoBehaviour
     public void RestartGame(){
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
-    
+    public void StartGame(){
+          isGameActive = true;
+        StartCoroutine(SpawnTarget());
+        score = 0;
+        UpdateScore(0);
+    }
 
 }
