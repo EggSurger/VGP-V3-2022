e7aaaac1090a4425de5fe5009d624741b5d0d38d
EggSurger <113132105+EggSurger@users.noreply.github.com>
2023-03-14 10:40:45 -0700

    suffering

--- games from class/Prototype4/Assets/Course Library/Scripts/RocketBehaviour.cs
+++ games from class/Prototype4/Assets/Course Library/Scripts/RocketBehaviour.cs
@@ -4,20 +4,26 @@ using UnityEngine;
 
 public class RocketBehaviour : MonoBehaviour
 {
-   public GameObject target;
-    public float speed = 10.0f;
-    public float speedRot = 10.0f;
+   //public GameObject target;
+    //public float speed = 10.0f;
+   // public float speedRot = 10.0f;
+private Transform target;
+private float speed = 15.0f;
+private bool homing = true;
 
+private float rocketStrength = 15.0f;
+private float aliveTimer = 5.0f;
     // Start is called before the first frame update
     void Start(){
-
+    
     }
 
     // Update is called once per frame
     void Update()
     {
-         if (!target) { Destroy(gameObject); return; }
-
+        /*
+         if (!target) { Destroy(gameObject);}
+; 
           Vector3 lookDirection = target.transform.position - transform.position;
 
         Quaternion rotateTarget = Quaternion.LookRotation(lookDirection, Vector3.up) * Quaternion.Euler(90.0f, 0, 0);
@@ -27,26 +33,33 @@ public class RocketBehaviour : MonoBehaviour
 
 
         transform.Translate(Vector3.up * Time.deltaTime * speed);
-
-
+        */
+if (homing && target !=null)
+{
+    Vector3 moveDirection = (target.transform.position - transform.position) .normalized;
+    transform.position += moveDirection * speed * Time.deltaTime;
+    transform.LookAt (target);
+}
     }
+ public void Fire(Transform newTarget)
+{
+    target = newTarget;
+    homing = true;
+    Destroy(gameObject, aliveTimer);
+}
+
+
+    
 
     private void OnCollisionEnter(Collision collision)
     {
 
         if (collision.gameObject.CompareTag("Enemy"))
         {
-            Destroy(gameObject);
-
-            Rigidbody enemyRigidbody = collision.gameObject.GetComponent<Rigidbody>();
-            Vector3 awayFromPlayer = collision.gameObject.transform.position - transform.position;
-
-            enemyRigidbody.AddForce(awayFromPlayer * 10.0f, ForceMode.Impulse);
-
-        }
-        else if(!collision.gameObject.CompareTag("Bullet"))
-        {
-            Destroy(gameObject);
+           Rigidbody targetRigidbody = collision.gameObject.GetComponent<Rigidbody>();
+           Vector3 away = -collision.contacts[0].normal;
+           targetRigidbody.AddForce(away * rocketStrength, ForceMode.Impulse);
+           Destroy(gameObject);
         }
     }
 }
--- games from class/Prototype4/Assets/Course Library/Scripts/SpawnManager.cs
+++ games from class/Prototype4/Assets/Course Library/Scripts/SpawnManager.cs
@@ -7,18 +7,33 @@ public class SpawnManager : MonoBehaviour
     public int enemyCount;
     public GameObject[] EnemeyPrefab;
     private float spawnRange = 9;
-    public int waveNumber = 0;
+    public int waveNumber = 1;
     public GameObject[] powerupPrefabs;
     
     // Start is called before the first frame update
     void Start()
-    {
+    {{
         int randomPowerup = Random.Range(0, powerupPrefabs.Length);
 Instantiate(powerupPrefabs[randomPowerup], GenerateSpawnPosition(),
 powerupPrefabs[randomPowerup].transform.rotation);
-        SpawnEnemyWave(waveNumber);
+        SpawnEnemyWave(waveNumber);}
     }
-    void SpawnEnemyWave(int enemiesToSpawn){
+    void Update()
+    {
+    enemyCount = FindObjectsOfType<Enemy>().Length;
+    if (enemyCount == 0)
+    {
+        waveNumber++;
+    }
+    if (waveNumber == 0)
+    {
+        SpawnEnemyWave (waveNumber);
+    }
+      int randomPowerup = Random.Range(0, powerupPrefabs.Length);
+     Instantiate(powerupPrefabs[randomPowerup], GenerateSpawnPosition(),
+     powerupPrefabs[randomPowerup].transform.rotation);
+    }
+    private void SpawnEnemyWave(int enemiesToSpawn){
         
         for(int i = 0; i < enemiesToSpawn; i++){
             int randomEnemy = Random.Range(0,EnemeyPrefab.Length);
@@ -27,24 +42,12 @@ powerupPrefabs[randomPowerup].transform.rotation);
     }
     
     // Update is called once per frame
-    void Update()
+   
+    private Vector3 GenerateSpawnPosition()
     {
-        enemyCount = GameObject.FindGameObjectsWithTag("Enemy").Length;
-
-        if (enemyCount == 0)
-         {
-            waveNumber ++;
-            SpawnEnemyWave(waveNumber);
-            int randomPowerup = Random.Range(0, powerupPrefabs.Length);
-Instantiate(powerupPrefabs[randomPowerup], GenerateSpawnPosition(),
-powerupPrefabs[randomPowerup].transform.rotation);
-
-         }
-    }
-    private Vector3 GenerateSpawnPosition(){
         float spawnPosX = Random.Range(-spawnRange, spawnRange);
         float spawnPosZ = Random.Range(-spawnRange, spawnRange);
         Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosZ);
         return randomPos;
     }
-}
+    }
